c7b766b4e9c4bbaf67f1bda4f1930725e9260e1a
marcosumpani <marcosumpani@gmail.com>
2024-07-02 20:40:18 -0300

    feat: wip tests

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,9 @@
+import { createInertiaApp } from "@inertiajs/react";
 export default function Welcome() {
-    return(<>
-    <h1>TESTE</h1>
-    </>)
+
+    return (
+        <>
+            <h1>TESTE</h1>
+        </>
+    );
 }
