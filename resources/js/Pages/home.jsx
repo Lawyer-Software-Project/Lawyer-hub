3d0bb057c6e897148f46ccdc2494c01e66e0f3dd
marcosumpani <marcosumpani@gmail.com>
2024-07-07 11:12:32 -0300

    fix: add text middle in image

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -6,7 +6,12 @@ import Button from '../components/button/button.jsx'
 function Home() {
     return (<>
         <div>
-            <img src="/assets/home/mainHome.png" alt="mainImg" className="w-full h-[75vh] object-cover object-left-top overflow-hidden" />
+            <div className="grid">
+                <h2 className="absolute z-10 self-center text-white text-5xl w-[40%] m-[8%]">“Na advocacia moderna, inovar é ser pioneiro na defesa dos direitos e na busca por justiça.”</h2>
+                <img src="/assets/home/mainHome.png" alt="mainImg"
+                    className="w-full h-[75vh] object-cover object-left-top overflow-hidden brightness-75" />
+            </div>
+
             <Card
                 title={'Advogados Qualificados e Verificados para ajudar no seu Problema!'}
                 imageSrc={'/assets/home/lawyer.png'}
@@ -14,6 +19,7 @@ function Home() {
                 colorWords={['Qualificados', 'Verificados', 'Problema!']}
                 description={'Conecte-se com diversos advogados de diferentes áreas de todo o Brasil!'}
             />
+
             <Card
                 title={'Velocidade na resolução do seu Problema!'}
                 imageSrc={'/assets/home/speed.png'}
@@ -21,6 +27,7 @@ function Home() {
                 colorWords={['Velocidade', 'Problema!']}
                 description={'Resolva seu caso com mais rapidez, sem demora exagerada para encontrar advogados!'}
             />
+
             <div className="flex  justify-center m-[5vh]">
                 <Button
                     text={'Começar!'}
