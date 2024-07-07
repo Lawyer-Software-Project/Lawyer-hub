a9a0e8fd69623263681f1dac88ab8d624c4cdf05
marcosumpani <marcosumpani@gmail.com>
2024-07-07 00:55:26 -0300

    feat: add homePage and respective components

--- resources/js/layouts/layout.jsx
+++ resources/js/layouts/layout.jsx
@@ -1,12 +1,14 @@
-import { Link } from '@inertiajs/react'
+import { Head } from '@inertiajs/react'
+import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
 
-export default function Layout({ children }) {
+export default function Layout({ children, title }) {
   return (
-    <main>
-      <header class="grid grid-cols-3">
-       
-      </header>
-      <article>{children}</article>
+    <main className="font-['CrimsonText-Regular',serif] bg-[#f8f8f8]">
+      <Head title={title} />
+      <Header/>
+      <article >{children}</article>
+      <Footer/>
     </main>
   )
 }
