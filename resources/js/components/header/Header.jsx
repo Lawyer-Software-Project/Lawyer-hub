412b1413faabaf0f3fddbc13c7755b752e089f57
PedroMaxis <pedro11sales14@gmial.com>
2024-11-12 02:01:46 -0300

    feat:update design and added chat layout

--- resources/js/components/header/Header.jsx
+++ resources/js/components/header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    if(menuOpen) {
+    if(document.getElementById("dropdownMenu").style.display == "none") {
       document.getElementById("dropdownMenu").style.display = "block";
       setMenuOpen(!menuOpen);
     }
@@ -36,9 +36,9 @@ export default function Header() {
             <span>Bem-vindo, {auth.user.name}!</span>
             <div className="flex self-end">
             <img onClick={toggleMenu} id="menu-button" aria-expanded="true" aria-haspopup="true" src={`https://ui-avatars.com/api/?name=${auth.user.name}&background=5e87c5&rounded=true&size=30`} className="ml-2 cursor-pointer" alt="" />
-            <div id="dropdownMenu" className="absolute hidden transition ease-in-out top-[2.2vw] right-[6rem] z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+            <div id="dropdownMenu" className="absolute hidden transition ease-in-out top-[2.2vw] right-[6rem] z-10 w-56 origin-top-right divide-y divide-gray-100 rounded-b-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
     <div className="py-1" role="none">
-      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Meu Perfil</a>
+      <a href="/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Meu Perfil</a>
       <a href="/cases" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-4">Meus Casos</a>
    </div>
     <div className="py-1" role="none">
