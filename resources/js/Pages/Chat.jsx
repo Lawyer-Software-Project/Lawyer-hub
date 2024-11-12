6a4697f51827fb4cc787487119e9626569b80775
PedroMaxis <pedro11sales14@gmail.com>
2024-11-12 08:58:25 -0300

    feat:updated profile page

--- resources/js/Pages/Chat.jsx
+++ resources/js/Pages/Chat.jsx
@@ -23,35 +23,15 @@ const ChatApp = () => {
   return (<>
     <Header/>
     <div className="flex h-screen overflow-hidden">
-      {/* Sidebar */}
-      <div className="w-1/4 bg-white border-r border-gray-300">
-
-        {/* Contact List */}
-        <div className="overflow-y-auto h-screen p-3 mb-9 pb-20">
-          {/* Contact Items */}
-          {['Alice','Martin'].map((name, index) => (
-            <div key={index} className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
-              <div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
-                <img src={`https://placehold.co/200x/${Math.random().toString(16).slice(2, 8)}/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato`} alt="User Avatar" className="w-12 h-12 rounded-full" />
-              </div>
-              <div className="flex-1">
-                <h2 className="text-lg font-semibold">{name}</h2>
-                <p className="text-gray-600">Sample message...</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
       {/* Main Chat Area */}
       <div className="flex-1">
         {/* Chat Header */}
-        <header className="bg-white p-4 text-gray-700">
+        <header className="bg-white pl-36 p-4 mt-16 text-gray-700">
           <h1 className="text-2xl font-semibold">Alice</h1>
         </header >
 
         {/* Chat Messages */}
-        <div className="h-screen overflow-y-auto p-4 pb-36">
+        <div className="h-screen overflow-y-auto pr-36 pl-36 p-4 pb-36">
           {/* Messages */}
           <div className="flex mb-4 cursor-pointer">
             <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
@@ -74,7 +54,7 @@ const ChatApp = () => {
         </div>
 
         {/* Chat Input */}
-        <footer className=" bg-white border-t border-gray-300 p-4 absolute bottom-0 w-3/4">
+        <footer className=" bg-white border-t border-gray-300 p-4 absolute bottom-0 w-full">
           <div className="flex items-center">
             <input type="text" placeholder="Digite sua Mensagem..." className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500" />
             <button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">Enviar</button>
--- resources/js/Pages/ProfilePage.jsx
+++ resources/js/Pages/ProfilePage.jsx
@@ -1,76 +1,105 @@
-import Layout from "@/Layouts/layout.jsx"
+import React, { useState } from "react";
+import Layout from "@/Layouts/layout.jsx";
 import { MdEmail } from "react-icons/md";
 import InputMask from 'react-input-mask';
 
-//ALGUEM TROCA ESSA FONTE PELO AMOR DE DEUS
+//ALGUEM TROCA A FONTE DO TEXTO PELO AMOR DE DEUS
 function Profile() {
+    const [isEditable, setIsEditable] = useState(false);
+
+    const handleEditButtonClick = () => {
+        setIsEditable(!isEditable);
+    };
+
     return (
         <>
             <section className="min-h-screen -mt-10 flex justify-center items-center">
                 <div className="h-[76vh] w-[60vw] bg-[#f4f4f4d8] rounded-lg mb-8">
                     <div className="w-full rounded-t-lg h-16 bg-gradient-to-r from-[#4485E7] to-[#8F8F8F] "></div>
-                        <div id="container" className="pr-6 pl-6 ">
-                            <div className="flex flex-row w-full mt-4">
-                                <img className="w-[120px] rounded-full object-cover h-[120px]" src="/profile.jpg" alt="" />
-                                <div className="flex flex-col ml-4 mt-4">
-                                <h1 className="text-2xl">Fulano da Silva</h1>
-                                <p className="font-light">email@email.com</p>
-                                <p className="font-bold">Advogado</p>
+                    <div id="container" className="pr-6 pl-6 ">
+                        <div className="flex flex-row w-full mt-4">
+                            <img className="w-[120px] rounded-full object-cover h-[120px]" src="/profile.jpg" alt="" />
+                            <div className="flex flex-col ml-4 mt-4 flex-1">
+                                <div className="mb-auto">
+                                    <h1 className="text-2xl">Fulano da Silva</h1>
+                                    <p className="font-light">email@email.com</p>
+                                    <p className="font-bold">Advogado</p>
                                 </div>
-                                <button className="text relative top-4 left-[400px] font-['Poppins',_sans-serif] w-[120px] bg-sky-500 text-white rounded-[9px] mt-3 text-[15px] h-[40px]"                                >Editar</button>
-                        </div>
-                        <div className="mt-6 grid gap-4 grid-cols-2">
-                                <div className="flex flex-col">
-                                    <label className="text-[#555555] text-left self-start text-base font-normal">Nome Completo</label>
-                                    <input placeholder="Nome Completo" disabled className="bg-[#eaeaea] border-0 rounded-md h-10" type="text" />
-                                </div>
-                                <div className="flex flex-col">
-                                    <label className="text-[#555555] text-left self-start text-base font-normal">Sua Senha</label>
-                                    <input placeholder="*************" disabled className="bg-[#eaeaea] border-0 rounded-md h-10" type="password" />
+                                <div className="flex justify-end">
+                                    <button
+                                        id="edtButton"
+                                        onClick={handleEditButtonClick}
+                                        className="font-['Poppins',_sans-serif] -mt-20 w-[120px] bg-sky-500 text-white rounded-[9px] text-[15px] h-[40px]"
+                                    >
+                                        {isEditable ? "Salvar" : "Editar"}
+                                    </button>
                                 </div>
                             </div>
+                        </div>
+
                         <div className="mt-6 grid gap-4 grid-cols-2">
-                                <div className="flex flex-col">
-                                    <label className="text-[#555555] text-left self-start text-base font-normal">Tipo Juridico</label>
-                                    <select className="bg-[#eaeaea] border-0 rounded-md h-10" name="" id="">
-                                    <option value="1">teste</option>  |  
-                                    <option value="2">teste2</option>  |  
-                                    <option value="3">teste3</option>  |  
-                                    <option value="4">teste4</option>  |  
-                                    </select>                                
-                                </div>
-                                <div className="flex flex-col">
-                                    <label className="text-[#555555] text-left self-start text-base font-normal">Reg. OAB</label>
-                                    <InputMask
+                            <div className="flex flex-col">
+                                <label className="text-[#555555] text-left self-start text-base font-normal">Nome Completo</label>
+                                <input
+                                    placeholder="Nome Completo"
+                                    disabled={!isEditable}
+                                    className="bg-[#eaeaea] border-0 rounded-md h-10"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <label className="text-[#555555] text-left self-start text-base font-normal">Sua Senha</label>
+                                <input
+                                    placeholder="*************"
+                                    disabled={!isEditable}
+                                    className="bg-[#eaeaea] border-0 rounded-md h-10"
+                                    type="password"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-6 grid gap-4 grid-cols-2">
+                            <div className="flex flex-col">
+                                <label className="text-[#555555] text-left self-start text-base font-normal">Tipo Juridico</label>
+                                <select
+                                    className="bg-[#eaeaea] border-0 rounded-md h-10"
+                                    disabled={!isEditable}
+                                >
+                                    <option value="1">teste</option>
+                                    <option value="2">teste2</option>
+                                    <option value="3">teste3</option>
+                                    <option value="4">teste4</option>
+                                </select>
+                            </div>
+                            <div className="flex flex-col">
+                                <label className="text-[#555555] text-left self-start text-base font-normal">Reg. OAB</label>
+                                <InputMask
                                     required
                                     mask="aa999999"
                                     name="usu_oab"
                                     className="bg-[#eaeaea] border-0 rounded-md h-10"
                                     placeholder="UF999999"
-                                    disabled
-                                    />
-                                </div>
-                                
+                                    disabled={!isEditable}
+                                />
                             </div>
+                        </div>
                         <div className="mt-8 flex flex-col justify-start">
-                               <h1 className="font-semibold mb-1">Meu Email</h1>
-                                <div className="flex flex-row items-center">
+                            <h1 className="font-semibold mb-1">Meu Email</h1>
+                            <div className="flex flex-row items-center">
                                 <div className="w-8 h-8 rounded-full flex justify-center items-center bg-[#bdd4f6]">
                                     <MdEmail className="fill-sky-500"></MdEmail>
                                 </div>
                                 <p className="font-light ml-2">email@email.com</p>
-                                </div>
-                                <p className="text-red-500 -mt-2 font-semibold ml-10">Não Verificado</p>
-                                <a className="bg-[#bdd4f6] rounded-lg text-center inline-block w-40 text-sky-500 font-semibold " href="#">Verificar</a>
-
+                            </div>
+                            <p className="text-red-500 -mt-2 font-semibold ml-10">Não Verificado</p>
+                            <a className="bg-[#bdd4f6] rounded-lg text-center inline-block w-40 text-sky-500 font-semibold " href="#">Verificar</a>
                         </div>
                     </div>
                 </div>
-
             </section>
-
-        </>)
+        </>
+    );
 }
-Profile.layout = page => <Layout children={page} title={'Profile'} fixedStatus={true} />
 
-export default Profile
+Profile.layout = page => <Layout children={page} title={'Profile'} fixedStatus={true} />;
+
+export default Profile;
